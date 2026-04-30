1d5dae5f8b35efeec20eb2343b753854164898ac
codewithme380@gmail.com <codewithme@gmail.com>
2026-04-30 22:21:13 +0500

    initial commit

---
+++ javacript/app.js
@@ -0,0 +1,29 @@
+// datasets: [{
+//   label: 'Sales',
+//   data: [10, 20, 15, 25, 30],
+//   borderColor: 'red',
+//   backgroundColor: 'rgba(0,0,255,0.1)',
+//   tension: 0.4, // smooth curve
+//   fill: true
+// }]
+// const ctx = document.getElementById('myChart');
+
+// new Chart(ctx, {
+//   type: 'line',
+//   data: {
+//     labels: ['Ali', 'Tue', 'Wed', 'Thu', 'Fri'],
+//     datasets: [{
+//       label: 'For me',
+//       data: [10, 20, 15, 25, 30],
+//       borderWidth: 1,
+//     }],
+//     datasets: [{
+//       label: 'For me',
+//       data: [10, 20, 15, 25, 30],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     responsive: true
+//   }
+// });
